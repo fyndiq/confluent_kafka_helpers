b5579988797962f5af287221aaec47ed40ab1ee4
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2017-08-21 14:50:55 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="confluent_kafka_helpers",
+      version="0.1",
+      description="Helpers for Confluent's Kafka Python client",
+      url="https://github.com/fyndiq/confluent_kafka_helpers",
+      author="Fyndiq AB",
+      author_email="support@fyndiq.com",
+      license="MIT",
+      packages=["confluent_kafka_helpers"],
+      zip_safe=False)
