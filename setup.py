175e78798a02d742bfcd1018a66a0bcb1037f708
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2017-10-03 10:28:09 +0200

    Require version >=0

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(name="confluent_kafka_helpers",
       install_requires=[
           'structlog>=17.2.0',
           'requests>=2.18.3',
-          'confluent-kafka[avro]>=0.0.0'
+          'confluent-kafka-python>=0'
       ],
       dependency_links=[
-          'git+https://github.com/confluentinc/confluent-kafka-python#egg=confluent-kafka[avro]-999.0.0'
+          'https://github.com/confluentinc/confluent-kafka-python/zipball/master#egg=confluent-kafka-python-0'
       ],
       zip_safe=False)
