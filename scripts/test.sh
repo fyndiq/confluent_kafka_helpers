20e74482fae24f8a89a50a48ca4ff8f2252fd2ff
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2017-10-23 16:02:31 +0200

    Remove token

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 [[ -z "${VIRTUAL_ENV}" ]] && . .venv/bin/activate
-pytest --cov=confluent_kafka_helpers/
+pytest -s --cov=confluent_kafka_helpers/
 
-#if [ "$1" == "ci" ]; then
-#    codecov -t $CODECOV_TOKEN
-#fi
+if [ "$1" == "ci" ]; then
+    codecov
+fi
