073bef7ad2d117491099dbaf109620effc881eca
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2018-01-18 16:39:04 +0100

    Handle keyboard interrupts

--- confluent_kafka_helpers/__init__.py
+++ confluent_kafka_helpers/__init__.py
@@ -1,15 +1,32 @@
 """
-Default atexit registered functions will not be triggered on signals.
+Default atexit registered functions will not be triggered on error signals.
 
-However - if we manually handle the signals and exit the functions will be
-triggered.
+However - if we manually handle the error signals and exit, the functions will
+be triggered.
 """
 import signal
 import sys
 
+import confluent_kafka
+import structlog
 
-def handler(signum, frame):
-    sys.exit(128 + signum)
+logger = structlog.get_logger(__name__)
+logger.info(
+    "Using confluent kafka versions", version=confluent_kafka.version(),
+    libversion=confluent_kafka.libversion()
+)
 
 
-signal.signal(signal.SIGTERM, handler)
+def error_handler(signum, frame):
+    logger.info("Received signal", signum=signum)
+    exit_code = 128 + signum
+    sys.exit(exit_code)
+
+
+def interrupt_handler(signum, frame):
+    logger.info("Received signal", signum=signum)
+    sys.exit(1)
+
+
+signal.signal(signal.SIGTERM, error_handler)  # graceful shutdown
+signal.signal(signal.SIGINT, interrupt_handler)  # keyboard interrupt
