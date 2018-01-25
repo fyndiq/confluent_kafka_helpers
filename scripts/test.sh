1fa4a458b63612848bed5e0f433abb221305c449
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2018-01-25 14:42:08 +0100

    Report missing coverage

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 set -e
 
 [[ -z "${VIRTUAL_ENV}" ]] && . .venv/bin/activate
-pytest --cov=confluent_kafka_helpers/ --junitxml=/tmp/test-results/report.xml
+pytest --cov=confluent_kafka_helpers/ --junitxml=/tmp/test-results/report.xml --no-cov-on-fail --cov-report term-missing
 
 if [ "$1" == "ci" ]; then
     codecov
