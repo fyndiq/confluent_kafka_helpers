c354195252091d276323441559973eb6c0c4236e
Sergei Beilin (Fyndiq AB) <sergei.beilin@fyndiq.com>
2018-04-04 13:07:17 +0200

    Bump version, (maybe) fix dependency link

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name="confluent_kafka_helpers",
-      version="0.5.2",
+      version="0.5.3",
       description="Helpers for Confluent's Kafka Python client",
       url="https://github.com/fyndiq/confluent_kafka_helpers",
       author="Fyndiq AB",
@@ -10,7 +10,10 @@ setup(name="confluent_kafka_helpers",
       packages=find_packages(),
       install_requires=[
           'structlog>=17.2.0',
-          'git+https://github.com/confluentinc/confluent-kafka-python@v0.11.4#egg=confluent-kafka[avro]',
+          'confluent-kafka[avro]==0.11.4',
           'fastavro[ujson]==0.16.7'
+      ],
+      dependency_links=[
+          'https://github.com/confluentinc/confluent-kafka-python/archive/v0.11.4.zip#egg=confluent-kafka[avro]'
       ],
       zip_safe=False)
