7424fc96ae48aa0e8a9ce598d58e0f8514ff6889
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2018-09-17 15:42:11 +0200

    Add tests

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,11 +1,16 @@
 from unittest.mock import MagicMock, Mock, patch
 
 import pytest
+from confluent_kafka import KafkaError as ConfluentKafkaError
+from confluent_kafka import KafkaException
 
 from confluent_kafka_helpers import loader
-from confluent_kafka_helpers.exceptions import EndOfPartition
+from confluent_kafka_helpers.exceptions import (
+    EndOfPartition, KafkaTransportError
+)
 
 from tests import config, conftest
+from tests.kafka import KafkaError
 
 mock_avro_consumer = conftest.ConfluentAvroConsumerMock(
     name='ConfluentAvroConsumerMock'
@@ -57,8 +62,6 @@ def test_default_partitioner(key, num_partitions, expected_response):
 
 @patch('confluent_kafka_helpers.loader.TopicPartition', mock_topic_partition)
 def test_avro_message_loader_load(avro_message_loader):
-    # TODO: refactor
-    # reset mock cause the messages has already been consumed at this point...
     message = conftest.PollReturnMock()
     conftest.mock_confluent_avro_consumer.poll.side_effect = [
         message, EndOfPartition
@@ -89,3 +92,25 @@ class TestFindDuplicatedMessages:
         loader.find_duplicated_messages(messages, logger)
 
         assert logger.critical.call_count == 1
+
+
+class TestErrorHandler:
+    def test_raises_endofpartition_on_kafkaerror_partition_eof(self):
+        error = KafkaError(_code=ConfluentKafkaError._PARTITION_EOF)
+        with pytest.raises(EndOfPartition):
+            loader.default_error_handler(error)
+
+    def test_raises_kafkatransporterror_on_kafkaerror_transport(self):
+        error = KafkaError(_code=ConfluentKafkaError._TRANSPORT)
+        with pytest.raises(KafkaTransportError):
+            loader.default_error_handler(error)
+
+    @pytest.mark.parametrize('code', [
+        (ConfluentKafkaError._ALL_BROKERS_DOWN),
+        (ConfluentKafkaError._NO_OFFSET),
+        (ConfluentKafkaError._TIMED_OUT)
+    ])
+    def test_raises_kafkaexception_on_other_errors(self, code):
+        error = KafkaError(_code=code)
+        with pytest.raises(KafkaException):
+            loader.default_error_handler(error)
