96eeace611b5166786b66ce6ffb2f31619335a5f
Simon Wahlgren <simon.wahlgren@gmail.com>
2020-04-22 10:44:34 +0200

    setup.py: pin confluent kafka to <1.4.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name="confluent-kafka-helpers",
       packages=find_packages(),
       install_requires=[
           'structlog>=17.2.0',
-          'confluent-kafka>=1.0.0',
+          'confluent-kafka>=1.0.0,<1.4.0',
           'fastavro>=0.18.0',
           'avro-python3>=1.8.2',
       ],
