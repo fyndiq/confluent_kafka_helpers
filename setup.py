f9a66e6d9f2bd905310db7d4be9187337a141fd5
Simon Wahlgren <simon.wahlgren@gmail.com>
2020-04-28 12:03:17 +0200

    setup.py: add wheel to setup_requires

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import find_packages, setup
 
-setup(name="confluent-kafka-helpers",
-      version="0.7.4",
-      description="Helpers for Confluent's Kafka Python client",
-      url="https://github.com/fyndiq/confluent_kafka_helpers",
-      author="Fyndiq AB",
-      author_email="support@fyndiq.com",
-      license="MIT",
-      packages=find_packages(),
-      install_requires=[
-          'structlog>=17.2.0',
-          'confluent-kafka>=1.0.0,<1.4.0',
-          'fastavro>=0.18.0',
-          'avro-python3>=1.8.2',
-      ],
-      zip_safe=False)
+setup(
+    name="confluent-kafka-helpers",
+    version="0.7.4",
+    description="Helpers for Confluent's Kafka Python client",
+    url="https://github.com/fyndiq/confluent_kafka_helpers",
+    author="Fyndiq AB",
+    author_email="support@fyndiq.com",
+    license="MIT",
+    packages=find_packages(),
+    setup_requires=['wheel'],
+    install_requires=[
+        'structlog>=17.2.0',
+        'confluent-kafka>=1.0.0,<1.4.0',
+        'fastavro>=0.18.0',
+        'avro-python3>=1.8.2',
+    ],
+    zip_safe=False,
+)
