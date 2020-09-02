fd0d23684c2feba9575d15b63d9ea8ee6131894a
Simon Wahlgren <simon.wahlgren@gmail.com>
2020-09-02 10:24:02 +0200

    setup.py: bump version to 0.7.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="confluent-kafka-helpers",
-    version="0.7.5",
+    version="0.7.6",
     description="Helpers for Confluent's Kafka Python client",
     url="https://github.com/fyndiq/confluent_kafka_helpers",
     author="Fyndiq AB",
