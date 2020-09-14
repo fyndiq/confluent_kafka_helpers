f15a4d3574925acfb5f2c04a9eba7766f244a524
Simon Wahlgren <simon.wahlgren@gmail.com>
2020-09-14 13:23:07 +0200

    consumer: set default auto.offset.reset to earliest

--- confluent_kafka_helpers/consumer.py
+++ confluent_kafka_helpers/consumer.py
@@ -54,7 +54,7 @@ class AvroConsumer:
     DEFAULT_CONFIG = {
         'client.id': socket.gethostname(),
         'default.topic.config': {
-            'auto.offset.reset': 'latest'
+            'auto.offset.reset': 'earliest'
         },
         'enable.auto.commit': False,
         'fetch.error.backoff.ms': 0,
