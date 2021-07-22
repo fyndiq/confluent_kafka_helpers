6702be71732c26a6fea15071420a36994267bd0e
Oscar Linderoth <oscar.linderoth@fyndiq.com>
2021-07-22 18:55:12 +0200

    Raise confluent-kafka pin to <=1.7.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     setup_requires=['wheel'],
     install_requires=[
         'structlog>=17.2.0',
-        'confluent-kafka>=1.0.0,<=1.6.1',
+        'confluent-kafka>=1.0.0,<=1.7.0',
         'fastavro>=0.18.0,<=1.1.0',
         'avro-python3>=1.8.2,<=1.10.0',
     ],
