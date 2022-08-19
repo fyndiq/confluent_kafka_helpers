7884fd5ae3b19dace3640faf2e68ffa9f9e50fd7
Simon Wahlgren <simon.wahlgren@gmail.com>
2022-08-19 13:53:53 +0200

    relax confluent-kafka constraints

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="confluent-kafka-helpers",
-    version="0.10.2",
+    version="0.10.3",
     description="Helpers for Confluent's Kafka Python client",
     url="https://github.com/fyndiq/confluent_kafka_helpers",
     author="Fyndiq AB",
@@ -12,9 +12,7 @@ setup(
     setup_requires=['wheel'],
     install_requires=[
         'structlog>=17.2.0',
-        'confluent-kafka>=1.0.0,<=1.9.0',
-        'fastavro>=0.18.0,<=1.5.1',
-        'avro-python3>=1.8.2,<=1.10.2',
+        'confluent-kafka>=1.0<=1.9',
     ],
     extras_require={'opentracing': 'opentracing>=2.4.0'},
     zip_safe=False,
