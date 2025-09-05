5e5e3986663e975fa360257b56ff944764806b10
Simon Wahlgren <simon.wahlgren@gmail.com>
2025-09-05 14:25:57 +0200

    fix: remove debug log at import time

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="confluent-kafka-helpers",
-    version="1.1.0",
+    version="1.1.1",
     description="Helpers for Confluent's Kafka Python client",
     url="https://github.com/fyndiq/confluent_kafka_helpers",
     author="Fyndiq AB",
